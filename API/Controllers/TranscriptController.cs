fcd6746c3fe98d39c59090c0f202cc86c86dc9e8
Hayden Setlik <haydensetlik@gmail.com>
2021-11-10 22:37:23 -0700

    abstract terms from transcript chunk works

--- API/Controllers/TranscriptController.cs
+++ API/Controllers/TranscriptController.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Application.DataObjectHandling.Transcripts;
+using Application.DataObjectHandling.Terms;
+using static Application.DataObjectHandling.Transcripts.GetAbstractTermsForChunk;
 
 namespace API.Controllers
 {
@@ -17,6 +19,13 @@ namespace API.Controllers
         public async Task<IActionResult> CreateTranscript(CreateTranscriptDto dto)
         {
             return HandleResult(await Mediator.Send(new CreateTranscript.Command{CreateTranscriptDto = dto}));
+        }
+
+        [Authorize]
+        [HttpPost("getAbstractTermsForChunk")]
+        public async Task<IActionResult> GetAbstractTermsForChunk(TranscriptChunkDto dto)
+        {
+            return HandleResult(await Mediator.Send(new GetAbstractTermsForChunk.Query{Dto = dto}));
         }
     }
 }
