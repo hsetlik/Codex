59389f554df5b9a55b5d06645e28a99186139e46
Hayden Setlik <haydensetlik@gmail.com>
2021-11-28 15:48:09 -0700

    added RatingButton component

---
+++ codex-client/src/components/content/RatingButton.tsx
@@ -0,0 +1,18 @@
+import { observer } from "mobx-react-lite";
+import React from "react";
+import { Button } from "semantic-ui-react";
+import { AbstractTerm } from "../../app/models/userTerm";
+
+interface Props {
+    ratingValue: number,
+    term: AbstractTerm
+}
+
+export default observer (function RatingButton({ratingValue, term}: Props) {
+
+    return (
+        <Button >
+
+        </Button>
+    )
+})
---
+++ codex-client/src/components/content/RatingButtonGroup.tsx
@@ -0,0 +1,12 @@
+import { observer } from "mobx-react-lite";
+import React from "react";
+import { Button } from "semantic-ui-react";
+
+
+export default observer(function RatingButtonGroup() {
+    return(
+        <div>
+            
+        </div>
+    )
+})
