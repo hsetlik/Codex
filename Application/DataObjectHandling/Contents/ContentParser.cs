35c4eb263c98b34ff7c45e1949c50e88a49109d7
Hayden Setlik <haydensetlik@gmail.com>
2021-11-29 21:28:45 -0700

    basic HTML parsing

--- Application/DataObjectHandling/Contents/ContentParser.cs
+++ Application/DataObjectHandling/Contents/ContentParser.cs
@@ -7,22 +7,48 @@ using System.Threading;
 using System.Threading.Tasks;
 using Application.Core;
 using Application.DomainDTOs;
+using HtmlAgilityPack;
 using MediatR;
 
 namespace Application.DataObjectHandling.Contents
 {
     public class ContentParser
     {
+        private static async Task<string> CallUrl(string fullUrl)
+        {
+            HttpClient client = new HttpClient();
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls13;
+            client.DefaultRequestHeaders.Accept.Clear();
+            var response = client.GetStringAsync(fullUrl);
+            return await response;
+        }
+        private static HtmlDocument ToHtmlDoc(string fullText)
+        {
+            var doc = new HtmlDocument();
+            doc.LoadHtml(fullText);
+            return doc;
+        }
         public static async Task<Result<ContentCreateDto>> ParseToContent(string url)
         {
-            var httpClient = new HttpClient();
-            var response = await httpClient.GetAsync(url);
-            if (response == null)
-                return Result<ContentCreateDto>.Failure("Could not get http response");
-            var responseType = response.Content.GetType();
-            Console.WriteLine($"Response type is: {responseType.ToString()}");
+            var html = await CallUrl(url);
+            var htmlDoc = ToHtmlDoc(html);
+            var paragraphs = htmlDoc.DocumentNode.Descendants("p").ToList();
+            var htmlNode = htmlDoc.DocumentNode.Descendants("html").FirstOrDefault();
+            var lang = htmlNode.GetAttributeValue("lang", "en");
+            string fullText = "";
+            foreach(var paragraph in paragraphs)
+            {
+                Console.Write($"Paragraph {paragraphs.IndexOf(paragraph)}: \n {paragraph.InnerText}");
+                fullText += paragraph.InnerText;
+            }
+            var headNode = htmlDoc.DocumentNode.Descendants("head").FirstOrDefault();
+
             return Result<ContentCreateDto>.Success(new ContentCreateDto
             {
+                ContentName = $"{headNode.Descendants("title").FirstOrDefault().InnerText}",
+                ContentType = $"{paragraphs.Count} paragraphs found",
+                Language = lang,
+                FullText = fullText
 
             });
         }
