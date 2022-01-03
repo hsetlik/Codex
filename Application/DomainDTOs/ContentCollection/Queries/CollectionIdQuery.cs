c9026f7824476bf58349a9a8e90da654a82d4e19
Hayden Setlik <haydensetlik@gmail.com>
2022-01-03 00:08:07 -0700

    collection CRUD endpoints

---
+++ Application/DomainDTOs/ContentCollection/Queries/CollectionIdQuery.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Application.DomainDTOs.ContentCollection.Queries
+{
+    public class CollectionIdQuery
+    {
+        public Guid ContentCollectionId { get; set; }
+    }
+}
