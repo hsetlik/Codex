37df0bdc56c61ae684518675a05e8e6671944fc3
Hayden Setlik <haydensetlik@gmail.com>
2022-01-17 12:01:59 -0700

    lazy loading on client side (much faster)

--- codex-client/src/components/contentFrame/CodexNode.tsx
+++ codex-client/src/components/contentFrame/CodexNode.tsx
@@ -6,6 +6,7 @@ import { useStore } from "../../app/stores/store";
 import TextElement from "../content/leftColumn/commonReader/textElement/TextElement";
 import '../styles/content.css';
 import { ElementType } from "htmlparser2";
+import { useInView } from "react-intersection-observer";
 
 
 interface NodeProps {
@@ -25,19 +26,20 @@ const fullInnerText = (node: DOMNode & Element): string => {
     return text;
 }
 export default observer(function CodexNode({sourceNode, className}: NodeProps) {
+    const {ref, inView} = useInView({threshold: 0.2});
     // return an empty div if this isn't a valid element
     let text = fullInnerText(sourceNode as Element);
     const {htmlStore: {currentElementsMap, loadElementTerms, currentPageContent}} = useStore();
     useEffect(() => {
-        if (!currentElementsMap.has(text) && sourceNode instanceof Element && text.length > 1) {
+        if (!currentElementsMap.has(text) && sourceNode instanceof Element && text.length > 1 && inView) {
             loadElementTerms({elementText: text, tag: sourceNode.tagName, contentUrl: currentPageContent?.contentUrl || 'null'})
 
         }
-    }, [ currentElementsMap, loadElementTerms, text, currentPageContent, sourceNode])
+    }, [ currentElementsMap, loadElementTerms, text, currentPageContent, sourceNode, inView])
     if (!(sourceNode instanceof Element)) {
         return (<></>);
     }
-    const contentNode = (sourceNode instanceof Element && currentElementsMap.has(text)) ? 
+   const contentNode = (sourceNode instanceof Element && currentElementsMap.has(text)) ? 
     (<TextElement terms={currentElementsMap.get(text)!} />) : 
     (<></>); 
     
@@ -45,56 +47,56 @@ export default observer(function CodexNode({sourceNode, className}: NodeProps) {
         case 'p' || 'b':
             
             return (
-                <div className={className || 'codex-element-p'} {...sourceNode.attributes}>
+                <div className={className || 'codex-element-p'} {...sourceNode.attributes} ref={ref}>
                     {contentNode}
                 </div>
             );
         case 'h1':
             return (
-                <h1 className={className || 'codex-element-h1'}>
+                <h1 className={className || 'codex-element-h1'}ref={ref}>
                     {contentNode}
                 </h1>
             )
         case 'h2':
             return (
-                <h2 className={className || 'codex-element-h2'}>
+                <h2 className={className || 'codex-element-h2'}ref={ref}>
                     {contentNode}
                 </h2>
             )
         case 'h3':
             return (
-                <h3 className={className || 'codex-element-h3'}>
+                <h3 className={className || 'codex-element-h3'}ref={ref}>
                     {contentNode}
                 </h3>
             )
         case 'a':
             return (
-                <div className={className || 'codex-element-div'} >
+                <div className={className || 'codex-element-div'} ref={ref}>
                     {contentNode}
                 </div>
             )
         case 'div':
             return (
-                <div className={className || 'codex-element-div'}>
+                <div className={className || 'codex-element-div'}ref={ref}>
                     {contentNode}
                 </div>
             )
         case 'span':
             return (
-                <span className={className || 'codex-element-span'}>
+                <span className={className || 'codex-element-span'}ref={ref}>
                     {contentNode}
                 </span>
             )
         case 'li':
             return (
-                <span className={className || 'codex-element-span'}>
+                <span className={className || 'codex-element-span'}ref={ref}>
                     {contentNode}
                 </span>
             )
         default:
             console.log(`Node with tag ${sourceNode.tagName} not rendered!`);
             return (
-                <div className={className || 'codex-element-div'}>
+                <div className={className || 'codex-element-div'}ref={ref}>
                     {contentNode}
                 </div>
             )
