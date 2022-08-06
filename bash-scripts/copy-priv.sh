9de1a2abcfe4b2751e2275926352931ca8e7d174
Hayden Setlik <haydensetlik@gmail.com>
2022-08-06 07:39:02 -0600

    new priv handling script and scheme

--- bash-scripts/copy-priv.sh
+++ bash-scripts/copy-priv.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # This copies local credential files to git-ignored paths inside the project. Useful for some git operations.
-KEY_SRC="../../codex-priv/codextranslation-69b55f865606.json"
+KEY_SRC="../../priv/api-keys.json"
 KEY_DEST="../CodexBackend/ApiKeys"
 
-CRED_SRC="../../codex-priv/PrivateData.cs"
+CRED_SRC="../../priv/PrivateData.cs"
 CRED_DEST="../CodexBackend/Domain/PrivateData"
 
 mkdir -p $KEY_DEST
