fb499ef0f2c4976097bc2d5e5434c3f2d1c427f3
Hayden Setlik <haydensetlik@gmail.com>
2022-08-13 16:07:32 -0600

    new login form

--- codex-client/src/components/account/LoginForm.tsx
+++ codex-client/src/components/account/LoginForm.tsx
@@ -1,34 +1,48 @@
-import { ErrorMessage, Form, Formik, } from "formik";
-import { Button, Header, Label } from "semantic-ui-react";
-import CodexTextInput from "../formComponents/CodexTextInput"; 
+import { FormikErrors, useFormik, } from "formik";
 import { useStore } from "../../app/stores/store";
 import { observer } from "mobx-react-lite";
-import { useNavigate } from "react-router";
+import CodexTextInput from "../formComponents/CodexTextInput";
+
+interface LoginProps {
+    email: string,
+    password: string
+}
 
 
 export default observer(function LoginForm() {
-    const {userStore} = useStore();
-    const navigate = useNavigate();
+    const store = useStore();
+    const _validate = (values: LoginProps) => {
+        let errors: FormikErrors<LoginProps> = {};
+        if (!values.email)
+            errors.email = "Not a valid email address"
+        if (!values.password)
+            errors.password = "Incorrect password"
+    }
+    const formik = useFormik<LoginProps>({
+        initialValues: {
+            email: '',
+            password: ''
+        },
+        onSubmit(values, formikHelpers) {
+            store.userStore.login({ email: values.email, password: values.password }).catch(pro => {
+                console.log("Login failed");
+            }).then(() => {
+                //redirect to feed here
+                console.log("login finished");
+            })
+        },
+        validate: _validate
+    });
+
     return (
-        <Formik
-            initialValues={{email: '', password: '', error: null}}
-            onSubmit={(
-                (values, {setErrors}) =>  userStore.login(values).then(() => navigate('/feed'))
-            .catch(error => setErrors({error: 'Invalid email or password'})))}
-        >
-            {({handleSubmit, isSubmitting, errors}) => (
-                <Form className='ui form' onSubmit={handleSubmit} autoComplete='off'>
-                    <Header as='h2' content='Login' color='teal' textAlign='center' />
-                    <CodexTextInput name='email' placeholder='Email' />
-                    <CodexTextInput name='password' placeholder='Password' type='password' />
-                    <ErrorMessage name='error' render={() => (
-                        <Label style={{marginBottom: 10}}  basic color='red' content={errors.error}/> )}
-                    />
-                    <Button loading={isSubmitting} positive content='Login' type='submit' fluid onClick={() => {
-                        let lang = userStore.selectedProfile?.language;
-                    }} />
-                </Form>
-            )}
-        </Formik>
+        <div className="container">
+            <form onSubmit={formik.handleSubmit} className="col input-group">
+                <CodexTextInput name="email" />
+                <CodexTextInput name="password" type="password" />
+                <button type="submit">Submit</button>
+            </form>
+
+        </div>
+
     )
 })
